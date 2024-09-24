59c1c5009e89d9eb8a3a862c3687e6b9c1a62ba9
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-09-23 18:22:32 -0600

    Create Reciduo0.c

---
+++ Reciduo0.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int numero = 1;
+
+    while (numero>0 && numero< 1000){
+      
+        if(numero % 7 ==0){
+        printf("%d \n ", numero);
+            
+
+        }
+    
+        numero ++;
+
+    }
+}
