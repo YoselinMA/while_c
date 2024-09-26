38f4103594d6b8099c61780635379dbe092e4867
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-09-25 19:45:51 -0600

    Create While_Arreglo.c

---
+++ While_Arreglo.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main (){
+   int miArreglito[100];
+    int i = 0;
+    while(i<100){
+        printf("2 * %d = %d\n", i, 2*i);
+       i++;
+
+    }
+}
